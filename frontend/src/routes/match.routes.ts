4f206c56c079426cf42b33d49520eafa20538f0b
Daniel <dfpires@gmail.com>
2025-11-03 16:28:38 -0300

    backend match

--- frontend/src/routes/match.routes.ts
+++
@@ -1,8 +0,0 @@
-// src/routes/match.routes.ts
-import { Router } from "express";
-import { MatchController } from "../controllers/match.controller";
-
-export const matchRoutes = Router();
-
-// GET /match/:candidatoId
-matchRoutes.get("/:candidatoId", MatchController.listarVagasCompativeis);
--- frontend/src/routes/subtipos.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { SubtiposController } from "../controllers/subtipos.controller";
-const router = Router();
-
-router.get("/:id", SubtiposController.getOne); // /subtipos/:id
-router.get("/", SubtiposController.list) // /subtipo
-router.post("/", SubtiposController.create);   // /subtipos
-
-export default router;
